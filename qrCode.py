107e7fe9e8ea2b501cecbf0274474882b6097126
{von} <105366808+victorgabrielnascimento@users.noreply.github.com>
2023-07-05 08:31:20 -0300

    Update

--- qrCode.py
+++ qrCode.py
@@ -1,5 +1,7 @@
 import qrcode
 
+#Here you can change every product name and code, they are here set just for a exemple for you understand how the code work in a qr_code list#
+
 information_link ={
     "Portifólio": "",
     "GitHub": "https://github.com/victorgabrielnascimento",
